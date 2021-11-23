95c94b87ebf8b6f6442a3d18a86ce1999569bfe0
97ale <92416062+97ale@users.noreply.github.com>
2021-11-22 20:54:31 -0600

    Cita y Bitacora

---
+++ MVCProyecto2/src/Modelo/Bitacora.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Alejandra Merino
+ */
+class Bitacora {
+  private int idBitacora;
+  private String fecha;
+  private String hora;
+  private String nombreAutor;
+  private int identificadorCita;
+
+  public int getIdBitacora() {
+    return idBitacora;
+  }
+
+  public String getFecha() {
+    return fecha;
+  }
+
+  public String getHora() {
+    return hora;
+  }
+
+  public String getNombreAutor() {
+    return nombreAutor;
+  }
+
+  public int getIdentificadorCita() {
+    return identificadorCita;
+  }
+
+  public void setIdBitacora(int idBitacora) {
+    this.idBitacora = idBitacora;
+  }
+
+  public void setFecha(String fecha) {
+    this.fecha = fecha;
+  }
+
+  public void setHora(String hora) {
+    this.hora = hora;
+  }
+
+  public void setNombreAutor(String nombreAutor) {
+    this.nombreAutor = nombreAutor;
+  }
+
+  public void setIdentificadorCita(int identificadorCita) {
+    this.identificadorCita = identificadorCita;
+  }
+  
+  
+}
