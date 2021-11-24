21358ee14f2ec52c9b6809734b4996ce423eebaa
97ale <92416062+97ale@users.noreply.github.com>
2021-11-23 22:52:55 -0600

    CtrlDiagnosticoTratamiento

---
+++ MVCProyecto2/src/Controlador/CtrlDiagnosticoTratamiento.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Modelo.Diagnostico;
+import Modelo.Tratamiento;
+import Modelo.ConsultaDiagnosticoTratamiento;
+import Vista.DiagnosticoTratamiento;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Alejandra Merino
+ */
+public class CtrlDiagnosticoTratamiento {
+  
+}
